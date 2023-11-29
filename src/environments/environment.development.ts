220753c44e1c6a563c052cc2c8fa14d67416e698
Eiddwenn <136834165+Eiddwenn@users.noreply.github.com>
2023-11-29 18:35:43 +0100

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,15 +1,5 @@
 export const environment = {
 
-    movieUrl: 'https://api.themoviedb.org/3/movie',
-
-    searchUrl: 'https://api.themoviedb.org/3/search/movie',
-
-    movieApiKey: '7af32505f66dcc3356c2fe7929ecfaab',
-
-    nowPlaying: 'now_playing',
-    popular: 'popular',
-    topRated: 'top_rated',
-    upcoming: 'upcoming'
 
     
 
